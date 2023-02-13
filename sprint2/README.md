01a21f5e479f8be892f4dce46f389de55d84127c
Taíssa Silva <61012521+sTaissa@users.noreply.github.com>
2023-02-13 08:17:06 -0300

    corrige exercicio 16

--- sprint2/README.md
+++ sprint2/README.md
@@ -289,7 +289,7 @@ Saída da query:
 
 ![saída exercício 15](/sprint2/imagens-sprint2/e15.PNG)
 
-### Exercício 15
+### Exercício 16
 Apresente a query para listar a quantidade média vendida de cada produto agrupado por estado da federação. As colunas presentes no resultado devem ser estado e nmprod e quantidade_media. Considere arredondar o valor da coluna quantidade_media na quarta casa decimal. Ordene os resultados pelo estado (1º) e nome do produto (2º).
 Obs: Somente vendas concluídas.
 ``` SQL
--- sprint2/SQL-exercicios-loja.sql
+++ sprint2/SQL-exercicios-loja.sql
@@ -113,7 +113,7 @@ WHERE deletado <> 0 --Seleciona as vendas onde o campo "deletado" é true (1)
 ORDER BY cdven ASC
 
 
--- E1: Apresente a query para listar a quantidade média vendida de cada produto agrupado por estado da federação. As colunas presentes 
+-- E16: Apresente a query para listar a quantidade média vendida de cada produto agrupado por estado da federação. As colunas presentes 
 --no resultado devem ser estado e nmprod e quantidade_media. Considere arredondar o valor da coluna quantidade_media na quarta 
 --casa decimal. Ordene os resultados pelo estado (1º) e nome do produto (2º).
 -- Obs: Somente vendas concluídas.
