ab5999a0d0f85d618cd2e61179df8cbf1b7002f3
Taíssa Silva <61012521+sTaissa@users.noreply.github.com>
2023-02-28 09:36:01 -0300

    desafio 4/5, filme mais frequente

--- sprint3/desafio/ETL.py
+++ sprint3/desafio/ETL.py
@@ -68,11 +68,54 @@ def maior_media(dados):
             ator = item['nome']
     return ator
 
+# retorna uma lista com o(s) filme(s) que mais aparece(m) e sua frequência
+def filme_frequente(dados):
+    # cria um dicionário com cada filme individual e a sua frequência
+    filmes = {}
+    for item in dados:
+        # se o filme não estiver no dicionário, adiciona, se já estiver, apenas incrementa sua frequência
+        if item['1 filme'] not in filmes:
+            filmes[item['1 filme']] = 1
+        else:
+            filmes[item['1 filme']] += 1
+    
+    # verifica qual o filme/filmes do dicionário com maior frequência
+    maior = 0
+    frequente = []
+    for filme, frequencia in filmes.items():
+        # se for encontrada uma frequência maior, limpa a lista e adiciona o filme com a maior frequência apenas
+        if frequencia > maior:
+            maior = frequencia
+            frequente = []
+            frequente.append(filme)
+        # se houver mais de 1 filme com a maior frequência atual, adiciona este à lista de filmes com maior frequência
+        elif frequencia == maior:
+            frequente.append(filme)
+    return frequente, maior
+
+# retorna uma string pronta para ser exibida em um print da lista passada
+def exibe_filmes(lista):
+    resposta = ""
+    c = 0
+    for i in lista:
+        # se for o último item da lista não coloca vírgula ao fim
+        if c != len(lista) - 1:
+            resposta += i + ", "
+        else:
+            resposta += i
+        c += 1
+    return resposta
+
 # exetuta funções
-ator, filmes = mais_filmes(extrai())
-#print(f"Ator/atriz com maior número de filmes: {ator}. Quantidade de filmes: {filmes}")
+dados = extrai()
 
-#print(f"A média do número de filmes por autor é: {media_filmes(extrai())}")
+ator, filmes = mais_filmes(dados)
+#print(f"Ator/atriz com maior número de filmes: {ator}. Quantidade de filmes: {filmes}\n")
 
-print(f"Ator/atriz com a maior média por filme: {maior_media(extrai())}")
+#print(f"A média do número de filmes por autor é: {media_filmes(dados)}\n")
+
+#print(f"Ator/atriz com a maior média por filme: {maior_media(dados)}\n")
+
+filme, frequencia = filme_frequente(dados)
+print(f"O(s) filme(s) mais frequente(s) é(são): {exibe_filmes(filme)}, e sua frequência é: {frequencia}\n")
 
