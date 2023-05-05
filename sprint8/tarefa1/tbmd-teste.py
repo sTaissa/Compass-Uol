0edb527fe45f199f04d3035035f9388b20446fdf
sTaissa <staissa002@gmail.com>
2023-05-05 08:47:37 -0300

    adiciona tarefa 1 sprint 8

---
+++ sprint8/tarefa1/tbmd-teste.py
@@ -0,0 +1,22 @@
+import requests
+import pandas as pd
+import apikey
+from IPython.display import display
+
+url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={apikey.api_key}&language=pt-BR"
+
+response = requests.get(url)
+data = response.json()
+
+filmes = []
+
+for movie in data['results']:
+    df = {'Titulo': movie['title'],
+    'Data de lançamento': movie['release_date'],
+    'Visão geral': movie['overview'],
+    'Votos': movie['vote_count'],
+    'Média de votos:': movie['vote_average']}
+    filmes.append(df)
+
+df = pd.DataFrame(filmes)
+display(df)
